4e9518721310ed148df77757ac49c44fab1801cb
Athan Clark <athan.clark@gmail.com>
2014-12-18 10:32:34 -0700

    test suite fully operational :E

--- test/MainSpec.hs
+++ test/MainSpec.hs
@@ -18,17 +18,26 @@ import Data.Monoid
 main :: IO ()
 main = hspec spec
 
+data Deploy = Rel
+            | Gro
+            | Abs
+
 spec :: Spec
 spec = do
   describe "rendering" $ do
     it "should intercalate `?` and `&`" $ property $
-      testPath runRelativeUrl
+      testPath runRelativeUrl Rel
+    it "should intercalate `?` and `&` and prepend `/`" $ property $
+      testPath runGroundedUrl Gro
+    it "should intercalate `?` and `&` and prepend the host" $ property $
+      testPath runAbsoluteUrl Abs
   where
-    testPath :: ( UrlReader m
-                , Url UrlString m ) =>
+    testPath :: ( UrlReader T.Text m
+                , Url T.Text m ) =>
                 ( m T.Text
                -> T.Text
-               -> T.Text )
+               -> T.Text ) -- runner
+             -> Deploy -- deployment scheme
              -> T.Text -- Host
              -> T.Text -- Target
              -> T.Text -- Key1
@@ -36,12 +45,17 @@ spec = do
              -> T.Text -- Key2
              -> T.Text -- Val2
              -> Property
-    testPath renderPath host target key1 val1 key2 val2 =
+    testPath renderPath d host target key1 val1 key2 val2 = do
+      let host' = case d of
+                    Rel -> ""
+                    Gro -> "/"
+                    Abs -> host <> "/"
+
       not (T.null host) ==>
-      not (T.null target) ==>
-      not (T.null key1) ==>
-      not (T.null val1) ==>
-      not (T.null key2) ==>
-      not (T.null val2) ==>
-        (renderPath (renderUrl $ target <?> (key1, val1) <&> (key2, val2)) host)
-        === (target <> "?" <> key1 <> "=" <> val1 <> "&" <> key2 <> "=" <> val2)
+        not (T.null target) ==>
+          not (T.null key1) ==>
+            not (T.null val1) ==>
+              not (T.null key2) ==>
+                not (T.null val2) ==>
+                  (renderPath (url $ target <?> (key1, val1) <&> (key2, val2)) host)
+                  === (host' <> target <> "?" <> key1 <> "=" <> val1 <> "&" <> key2 <> "=" <> val2)
